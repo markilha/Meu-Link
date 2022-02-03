339b67791f8d0f139597e48ed3a8b6b5519b9769
Marco Antonio Monteiro de Almeida <malmeida@itesp.sp>
2022-02-03 14:50:13 -0300

    Finalizando página not found

--- src/routers.js
+++ src/routers.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Error from './pages/Error';
 import Links from './pages/Links';
-
 
 function RoutesApp() {
 
@@ -10,6 +10,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/links" element={<Links />} />
+                <Route path="*" element={<Error/>} />
             </Routes>
         </BrowserRouter>
     )
