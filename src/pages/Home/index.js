f657925caf9e8403d77c9ca573cc6d48f3467b55
Marco Antonio Monteiro de Almeida <malmeida@itesp.sp>
2022-02-03 15:20:26 -0300

    Finalizando o modal encurtar link

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import {FiLink} from 'react-icons/fi';
 import './home.css';
 import Menu from '../../components/Menu';
+import LinkItem from '../../components/LinkItem'
+
 
 export default function Home() {
+  const[link,setLink] = useState('');
+
+  function handleShortLink(){
+    alert('clicou')
+  }
 
   return (
     <div className="container-home" >
@@ -17,11 +25,16 @@ export default function Home() {
           <FiLink size={24} color="#FFF"/>
           <input
           placeholder='Cole seu link aqui...'
+          value={link}
+          onChange={(e)=> setLink(e.target.value)}
           />
         </div>
-        <button>Encurtar Link</button>
+        <button
+        onClick={handleShortLink}
+        >Encurtar Link</button>
       </div>
       <Menu/>
+      <LinkItem/>
     </div>
 
   )
